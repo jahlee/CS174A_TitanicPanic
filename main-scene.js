750c1a893782e0cb28a804380ff7f7d49755a358
stephanieshin1 <49211123+stephanieshin1@users.noreply.github.com>
2020-11-16 16:57:56 -0800

    initial project proposal
    - 3 different types of moving planets
    - light source at the rocket

--- main-scene.js
+++ main-scene.js
@@ -6,7 +6,7 @@ import {Obj_File_Demo} from "./examples/obj-file-demo.js"
 import {Scene_To_Texture_Demo} from "./examples/scene-to-texture-demo.js"
 import {Surfaces_Demo} from "./examples/surfaces-demo.js"
 import {Text_Demo} from "./examples/text-demo.js"
-import {Transforms_Sandbox} from "./examples/transforms-sandbox.js"
+import {main_game} from "./main-game.js"
 // Pull these names into this module's scope for convenience:
 const {
     Vector, Vector3, vec, vec3, vec4, color, Matrix, Mat4, Light, Shape, Material, Shader, Texture, Scene,
@@ -31,13 +31,13 @@ Object.assign(defs,
             {Scene_To_Texture_Demo},
             {Surfaces_Demo},
             {Text_Demo},
-            {Transforms_Sandbox});
+            {main_game});
 
 // ******************** End extra step
 
 // (Can define Main_Scene's class here)
 
-const Main_Scene = Transforms_Sandbox;
+const Main_Scene = main_game;
 const Additional_Scenes = [];
 
 export {Main_Scene, Additional_Scenes, Canvas_Widget, Code_Widget, Text_Widget, defs}
